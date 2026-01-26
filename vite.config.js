cc8b9ef5ccca67c4a6929cfa5d24a13754120a6f
hsmorenom <sebas03272009@hotmail.com>
2026-01-26 18:17:32 -0500

    restore to post in vencel

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [tailwindcss()],
-    base: '/Portafolio_Sebastian_Moreno/',
-    build: {
-        outDir: 'docs'
-    }
+  plugins: [tailwindcss()],
 })
+
